f28f27acccec8f15ac7325978d8cbca8c8506181
Rich Polanco <rich.polanco@automattic.com>
2023-06-15 19:42:13 -0600

    Initial project

---
+++ GridCalendar/GridCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridCalendarApp.swift
+//  GridCalendar
+//
+//  Created by Rich Polanco on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct GridCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
